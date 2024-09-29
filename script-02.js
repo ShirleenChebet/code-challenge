63f0265d356c8397cfedbab4890ef3c1cfc8ff9c
ShirleenChebet <shirlynshirley9@gmail.com>
2024-09-29 06:55:18 +0300

    script-02.js

--- script-02.js
+++ script-02.js
@@ -1,16 +1,3 @@
-// let speed = parseInt(prompt("Enter the speed of the car:"));
-// const speedLimit = 70;
-
-// if (speed < speedLimit) {
-//     console.log("Ok");
-// } else {
-//     let demeritPoints = Math.floor((speed - speedLimit) / 5);
-//     console.log(`Points: ${demeritPoints}`);
-
-//     if (demeritPoints > 12) {
-//         console.log("License suspended");
-//     }
-// }
 const input = prompt("Enter speed of the car")
 const speed = parseInt(input, 10)
 
