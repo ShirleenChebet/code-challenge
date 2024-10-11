359bf98a13feadb2a3b1b3134079a36efa7f1558
ShirleenChebet <shirlynshirley9@gmail.com>
2024-10-11 12:11:25 +0300

    Update script-02.js

--- script-02.js
+++ script-02.js
@@ -1,35 +1,27 @@
-const input = prompt("Enter speed of the car")
-const speed = parseInt(input, 10)
+//Prompt user for speed input
+const input = prompt("Enter the speed of the car")
+const speed = parseInt(input, 10);
 
-// Validate input
-if (isNaN(speed) || speed < 0){
-    console.log("Invalid input. Please enter a valid input")
+//validate input
+if (isNaN(speed) || speed < 0) {
+    console.log("Invalid input. Please enter a valid input.")
 } else {
     calculateDemeritPoints(speed);
 }
 
-//Function to calculate demerit points based on speed
+//Function to calculate demerit points based on
 function calculateDemeritPoints(speed) {
     const speedLimit = 70;
 
     if (speed < speedLimit) {
-        console.log("ok");
+        console.log("Ok")
     } else {
-       const excessSpeed = speed - speedLimit;
-       const demeritPoints = Math.floor(excessSpeed)
+        const excessSpeed = speed - speedLimit;
+        const demeritPoints = Math.floor(excessSpeed)
 
-       console.log(`Points: ${demeritPoints}`);
-    
-       if (demeritPoints > 12) {
-          console.log("License Suspended")
-       }
-    } 
+        console.log('Points: ${demeritPoints}');
+        if (demeritPoints > 12) {
+            console.log("License suspended");
+        }
+    }
 }
-
-
-
-
-
-
-
-
